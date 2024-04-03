5378a621647f78ae20ae2c0d4e3c894dd8cb66e9
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-04-03 11:42:02 +0700

    set up cli for gRPC server

--- server/apps/job-application-service/src/main.ts
+++ server/apps/job-application-service/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { JobApplicationServiceModule } from './job-application-service.module';
 import { join } from 'path';
 import { Transport } from '@nestjs/microservices';
+import { JOBAPPLICATION_PACKAGE_NAME } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(JobApplicationServiceModule,{
     transport: Transport.GRPC,
     options: {
       url: '0.0.0.0:50051',
-      package: 'job_application',
+      package: JOBAPPLICATION_PACKAGE_NAME,
       protoPath: join(__dirname, './proto/job-application.proto')
   }});
   await app.listen();
