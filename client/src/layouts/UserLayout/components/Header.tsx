1008451261abadf5ecf07846341502c8cdb19b19
TRUONGKHOA <phamtruongkhoa2000@gmail.com>
2024-04-07 03:10:49 +0700

    add basic menutop

--- client/src/layouts/UserLayout/components/Header.tsx
+++ client/src/layouts/UserLayout/components/Header.tsx
@@ -1,5 +1,29 @@
+import '../../../App.css';
+
 function Header() {
-  return <div>header of user</div>;
+  return (
+    <header className="top-menu">
+      <div className="container">
+        <div className="logo">
+          logo
+        </div>
+        <nav className="nav-menu">
+          <ul>
+            <li><a href="#">Tìm Việc</a></li>
+            <li><a href="#">Ứng Viên</a></li>
+            <li><a href="#">Doanh Nghiệp</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Giới Thiệu</a></li>
+            <li><a href="#">Liên Hệ</a></li>
+          </ul>
+        </nav>
+        <div className="user-actions">
+          <a href="#">Đăng Nhập</a>
+          <a href="#">Đăng Ký</a>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
