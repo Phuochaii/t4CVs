b3e12c4ba03eb1482043d232f7e29892472c189d
Michael Thinh <michaelthinh1008@gmail.com>
2024-04-10 18:09:17 +0700

    thinh hot fix

--- client/src/layouts/HRLayout/components/SideBar.tsx
+++ client/src/layouts/HRLayout/components/SideBar.tsx
@@ -1,205 +1,221 @@
 import * as React from "react";
 import Icon from "../../../shared/components/regular-icon";
 import {
-  LucideProps,
-  LayoutGrid,
-  Gem,
-  Gift,
-  Bot,
-  BriefcaseBusiness,
-  File,
-  User,
-  LineChart,
-  ShoppingCart,
-  WandSparkles,
-  BadgePercent,
-  History,
-  Settings,
-  Bell,
-  Mail,
+    LucideProps,
+    LayoutGrid,
+    Gem,
+    Gift,
+    Bot,
+    BriefcaseBusiness,
+    File,
+    User,
+    LineChart,
+    ShoppingCart,
+    WandSparkles,
+    BadgePercent,
+    History,
+    Settings,
+    Bell,
+    Mail,
 } from "lucide-react";
 const Item = ({
-  _icon,
-  iconSize = 16,
+    _icon,
+    iconSize = 16,
 
-  title,
-  // subItems = [],
+    title,
+    // subItems = [],
 }: {
-  _icon?: React.FC<LucideProps>;
-  iconSize?: number;
-  title: string;
-  // subItems?: { icon?: string; title: string }[];
+    _icon?: React.FC<LucideProps>;
+    iconSize?: number;
+    title: string;
+    // subItems?: { icon?: string; title: string }[];
 }) => {
-  return (
-    <li style={{ padding: " 12px 14px", margin: 0 }}>
-      <a href="#" className="flex items-center space-x-2">
-        {_icon && (
-          <div
-            style={{
-              // width: "32px",
-              textAlign: "center",
-              marginRight: "10px",
-              marginLeft: "10px",
-            }}
-          >
-            <Icon icon={_icon} size={iconSize} color="black" />
-            {/* <i
+    return (
+        <li style={{ padding: " 12px 14px", margin: 0 }}>
+            <a href="#" className="flex items-center space-x-2">
+                {_icon && (
+                    <div
+                        style={{
+                            // width: "32px",
+                            textAlign: "center",
+                            marginRight: "10px",
+                            marginLeft: "10px",
+                        }}
+                    >
+                        <Icon icon={_icon} size={iconSize} color="black" />
+                        {/* <i
               className={icon}
               style={{
                 fontSize: "16px",
               }}
             ></i> */}
-          </div>
-        )}
+                    </div>
+                )}
 
-        <span>{title}</span>
-        {/* {subItems.length > 0 && (
+                <span>{title}</span>
+                {/* {subItems.length > 0 && (
           <div className="flex-grow flex justify-end">
             <i className="fa-solid fa-angle-right"></i>
           </div>
         )} */}
-      </a>
-    </li>
-  );
+            </a>
+        </li>
+    );
 };
 
 const Sidebar: React.FC = () => {
-  //
-  const sidebar_items = [
-    [
-      { _icon: LayoutGrid, title: "Bảng tin" },
-      {
-        _icon: Gem,
-        title: "TopCV Rewards",
-      },
-      { _icon: Gift, title: "Đổi quà" },
-      { _icon: Bot, icon: "fa-solid fa-robot", title: "Toppy AI - Đề xuất" },
-    ],
+    //
+    const sidebar_items = [
+        [
+            { _icon: LayoutGrid, title: "Bảng tin" },
+            {
+                _icon: Gem,
+                title: "TopCV Rewards",
+            },
+            { _icon: Gift, title: "Đổi quà" },
+            {
+                _icon: Bot,
+                icon: "fa-solid fa-robot",
+                title: "Toppy AI - Đề xuất",
+            },
+        ],
 
-    [
-      {
-        _icon: BriefcaseBusiness,
-        title: "Chiến dịch tuyển dụng",
-      },
-      { _icon: File, title: "Tin tuyển dụng" },
-      {
-        _icon: User,
-        title: "Quản lí CV",
-        sub_items: [{ icon: "", title: "Quản lí nhãn CV" }],
-      },
-      {
-        _icon: LineChart,
-        title: "Báo cáo tuyển dụng",
-      },
-    ],
+        [
+            {
+                _icon: BriefcaseBusiness,
+                title: "Chiến dịch tuyển dụng",
+            },
+            { _icon: File, title: "Tin tuyển dụng" },
+            {
+                _icon: User,
+                title: "Quản lí CV",
+                sub_items: [{ icon: "", title: "Quản lí nhãn CV" }],
+            },
+            {
+                _icon: LineChart,
+                title: "Báo cáo tuyển dụng",
+            },
+        ],
 
-    [
-      {
-        _icon: ShoppingCart,
-        title: "Mua dịch vụ",
-      },
-      {
-        _icon: WandSparkles,
-        title: "Dịch vụ của tôi",
-      },
-      {
-        _icon: BadgePercent,
-        title: "Mã ưu đãi",
-      },
-      { _icon: File, icon: "fa-solid fa-file", title: "Theo dõi đơn hàng" },
-    ],
-    [
-      {
-        _icon: History,
-        icon: "fa-solid fa-clock-rotate-left",
-        title: "Lịch sử hoạt động",
-      },
-      { _icon: Settings, icon: "fa-solid fa-gear", title: "Cài đặt tài khoản" },
-    ],
-    [
-      {
-        _icon: Bell,
-        icon: "fa-regular fa-bell",
-        title: "Thông báo hệ thống",
-      },
-      { _icon: Mail, icon: "fa-regular fa-envelope", title: "Nộp thư hỗ trợ" },
-    ],
-  ];
+        [
+            {
+                _icon: ShoppingCart,
+                title: "Mua dịch vụ",
+            },
+            {
+                _icon: WandSparkles,
+                title: "Dịch vụ của tôi",
+            },
+            {
+                _icon: BadgePercent,
+                title: "Mã ưu đãi",
+            },
+            {
+                _icon: File,
+                icon: "fa-solid fa-file",
+                title: "Theo dõi đơn hàng",
+            },
+        ],
+        [
+            {
+                _icon: History,
+                icon: "fa-solid fa-clock-rotate-left",
+                title: "Lịch sử hoạt động",
+            },
+            {
+                _icon: Settings,
+                icon: "fa-solid fa-gear",
+                title: "Cài đặt tài khoản",
+            },
+        ],
+        [
+            {
+                _icon: Bell,
+                icon: "fa-regular fa-bell",
+                title: "Thông báo hệ thống",
+            },
+            {
+                _icon: Mail,
+                icon: "fa-regular fa-envelope",
+                title: "Nộp thư hỗ trợ",
+            },
+        ],
+    ];
 
-  return (
-    <div
-      className="bg-white w-64 flex flex-col"
-      style={{
-        fontWeight: "500",
-        width: "260px",
-        color: "#212F3FE4",
-        height: "100vh",
-        overflowY: "scroll",
-      }}
-    >
-      <div className="flex" style={{ padding: " 10px 14px", margin: 0 }}>
-        <a href="#" className="flex items-center space-x-2">
-          <img
-            src="https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg"
-            className="rounded-full"
-            style={{ width: "32px", marginRight: "10px" }}
-            alt="Avatar"
-          />
+    return (
+        <div
+            className="bg-white w-64 flex flex-col"
+            style={{
+                fontWeight: "500",
+                minWidth: "260px",
+                color: "#212F3FE4",
+                height: "100vh",
+                overflowY: "scroll",
+            }}
+        >
+            <div className="flex" style={{ padding: " 10px 14px", margin: 0 }}>
+                <a href="#" className="flex items-center space-x-2">
+                    <img
+                        src="https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg"
+                        className="rounded-full"
+                        style={{ width: "32px", marginRight: "10px" }}
+                        alt="Avatar"
+                    />
 
-          <div>
-            <p>Moira Vien</p>
-            <p style={{ fontSize: "12px" }}>Employer</p>
-            <p style={{ fontSize: "12px" }}>
-              Tài khoản xác thực:{" "}
-              <span className="text-green-600 mr-2">Cấp 1/5</span>
-              <i
-                className="fa-solid fa-circle-question"
-                style={{ color: "#757575B9", fontSize: "16px" }}
-              ></i>
-            </p>
-          </div>
-        </a>
-      </div>
-      <button
-        className="btn mx-4 text-sm py-1 rounded-full w-11/12"
-        style={{
-          backgroundColor: "#EBF3FF",
-          color: "#2D7CF1",
-          marginBottom: "15.96px",
-        }}
-      >
-        <i
-          className="fa-solid fa-shield-halved"
-          style={{ fontSize: "14px" }}
-        ></i>
-        <span className="ml-2 mr-3">Xác nhận tài khoản điện tử</span>
-        <i
-          className="fa-solid fa-angles-right slide-animation"
-          style={{ fontSize: "14px" }}
-        ></i>
-      </button>
-      {sidebar_items.map((items, index) => {
-        return (
-          <ul
-            className="space-y-2"
-            key={index}
-            style={{ borderBottom: "1px solid #75757556" }}
-          >
-            {items.map((item, index_1) => {
-              return (
-                <Item
-                  _icon={item._icon}
-                  title={item.title}
-                  key={index_1}
+                    <div>
+                        <p>Moira Vien</p>
+                        <p style={{ fontSize: "12px" }}>Employer</p>
+                        <p style={{ fontSize: "12px" }}>
+                            Tài khoản xác thực:{" "}
+                            <span className="text-green-600 mr-2">Cấp 1/5</span>
+                            <i
+                                className="fa-solid fa-circle-question"
+                                style={{ color: "#757575B9", fontSize: "16px" }}
+                            ></i>
+                        </p>
+                    </div>
+                </a>
+            </div>
+            <button
+                className="btn mx-4 text-sm py-1 rounded-full w-11/12"
+                style={{
+                    backgroundColor: "#EBF3FF",
+                    color: "#2D7CF1",
+                    marginBottom: "15.96px",
+                }}
+            >
+                <i
+                    className="fa-solid fa-shield-halved"
+                    style={{ fontSize: "14px" }}
+                ></i>
+                <span className="ml-2 mr-3">Xác nhận tài khoản điện tử</span>
+                <i
+                    className="fa-solid fa-angles-right slide-animation"
+                    style={{ fontSize: "14px" }}
+                ></i>
+            </button>
+            {sidebar_items.map((items, index) => {
+                return (
+                    <ul
+                        className="space-y-2"
+                        key={index}
+                        style={{ borderBottom: "1px solid #75757556" }}
+                    >
+                        {items.map((item, index_1) => {
+                            return (
+                                <Item
+                                    _icon={item._icon}
+                                    title={item.title}
+                                    key={index_1}
 
-                  // subItems={item.sub_items}
-                />
-              );
+                                    // subItems={item.sub_items}
+                                />
+                            );
+                        })}
+                    </ul>
+                );
             })}
-          </ul>
-        );
-      })}
-    </div>
-  );
+        </div>
+    );
 };
 export default Sidebar;
--- client/src/pages/HR/PostJob.tsx
+++ client/src/pages/HR/PostJob.tsx
@@ -1,74 +1,153 @@
 // Thịnh
+
+import { useState } from "react";
 import "./PostJob.css";
 
 function PostJob() {
+    const [modal, setModal] = useState(false);
+    const toggleModal = () => {
+        setModal((prev) => !prev);
+    };
     return (
-        <div className="background">
-            <div className="post">
-                <div className="post_banner">
-                    <div className="banner_img">
-                        <img
-                            src="https://tuyendung.topcv.vn/app/_nuxt/img/banner-campaign.6a2c052.png"
-                            alt="campaign"
-                        />
+        <>
+            <div className="background">
+                <div className="post">
+                    <div className="post_banner">
+                        <div className="banner_img">
+                            <img
+                                src="https://tuyendung.topcv.vn/app/_nuxt/img/banner-campaign.6a2c052.png"
+                                alt="campaign"
+                            />
+                        </div>
+                        <div className="banner_info">
+                            Hoạt động tuyển dụng của doanh nghiệp được tiến hành
+                            theo từng giai đoạn
+                            <br /> với các mục tiêu tuyển dụng khác nhau. Chiến
+                            dịch tuyển dụng là nơi tổng hợp các
+                            <br /> hoạt động khác nhau của một đợt tuyển dụng
+                            được thực hiện trên nền tảng TopCV
+                        </div>
+                        <div className="banner_button">
+                            <button className="button">Tìm hiểu thêm</button>
+                        </div>
                     </div>
-                    <div className="banner_info">
-                        Hoạt động tuyển dụng của doanh nghiệp được tiến hành
-                        theo từng giai đoạn
-                        <br /> với các mục tiêu tuyển dụng khác nhau. Chiến dịch
-                        tuyển dụng là nơi tổng hợp các
-                        <br /> hoạt động khác nhau của một đợt tuyển dụng được
-                        thực hiện trên nền tảng TopCV
-                    </div>
-                    <div className="banner_button">
-                        <button className="button">Tìm hiểu thêm</button>
-                    </div>
-                </div>
-                <div className="post_info">
-                    <div className="info_main">
-                        Tạo chiến dịch tuyển dụng của bạn
-                    </div>
-                    <div className="info_form">
-                        <div className="form_item">
-                            <div className="item_label">
-                                Tên chiến dịch tuyển dụng *
+                    <div className="post_info">
+                        <div className="info_main">
+                            Tạo chiến dịch tuyển dụng của bạn
+                        </div>
+                        <div className="info_form">
+                            <div className="form_item">
+                                <div className="item_label">
+                                    Tên chiến dịch tuyển dụng *
+                                </div>
+                                <div className="item_input">
+                                    <input
+                                        autoFocus
+                                        type="text"
+                                        placeholder="VD: Tuyển dụng nhân viên Marketing tháng 10..."
+                                    />
+                                </div>
+                                <div className="item_error">
+                                    Tên chiến dịch tuyển dụng không được để
+                                    trống
+                                </div>
                             </div>
-                            <div className="item_input">
-                                <input
-                                    autoFocus
-                                    type="text"
-                                    placeholder="VD: Tuyển dụng nhân viên Marketing tháng 10..."
-                                />
-                            </div>
-                            <div className="item_error">
-                                Tên chiến dịch tuyển dụng không được để trống
+                            <div onClick={toggleModal} className="form_button">
+                                Tiếp theo
                             </div>
                         </div>
-                        <div className="form_button">Tiếp theo</div>
-                    </div>
-                    <div className="info_ref">
-                        <div className="ref_head">Tài liệu bạn nên xem</div>
-                        <div className="ref_description">
-                            Hiểu về cách chiến dịch tuyển dụng hoạt động sẽ giúp
-                            bạn tối ưu tốt hơn hoạt động tuyển dụng của doanh
-                            nghiệp trên TopCV. Hãy chắc chắn bạn đã tìm hiểu
-                            thông tin về chiến dịch tuyển dụng.
-                        </div>
-                        <div className="ref_links">
-                            <div className="link_item">
-                                Smart Recruitment Platform Principle
+                        <div className="info_ref">
+                            <div className="ref_head">Tài liệu bạn nên xem</div>
+                            <div className="ref_description">
+                                Hiểu về cách chiến dịch tuyển dụng hoạt động sẽ
+                                giúp bạn tối ưu tốt hơn hoạt động tuyển dụng của
+                                doanh nghiệp trên TopCV. Hãy chắc chắn bạn đã
+                                tìm hiểu thông tin về chiến dịch tuyển dụng.
                             </div>
-                            <div className="link_item">
-                                Khái niệm Chiến dịch tuyển dụng
-                            </div>
-                            <div className="link_item">
-                                Khởi tạo Chiến dịch tuyển dụng đúng cách
+                            <div className="ref_links">
+                                <div className="link_item">
+                                    Smart Recruitment Platform Principle
+                                </div>
+                                <div className="link_item">
+                                    Khái niệm Chiến dịch tuyển dụng
+                                </div>
+                                <div className="link_item">
+                                    Khởi tạo Chiến dịch tuyển dụng đúng cách
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {modal && (
+                <div className="modal">
+                    <div className="overlay" onClick={toggleModal}></div>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h2 className="modal-title">
+                                Khởi động chiến dịch
+                            </h2>
+                            <div className="close-btn" onClick={toggleModal}>
+                                X
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="p-6 flex justify-center flex-col ">
+                            <div className="text-2xl text-black font-semibold text-center">
+                                Chọn hoạt động tuyển dụng
+                            </div>
+                            <div className="text-base  text-neutral-700 text-center">
+                                Chúng tôi gợi ý những hoạt động bên dưới. Hãy
+                                lựa chọn hoạt động phù hợp.
+                            </div>
+                        </div>
+                        <div className="modal-context">
+                            <div className="col-md-5 w-[40%] px-4 ">
+                                <div className="cursor-pointer post-job-container py-6 px-5 rounded-lg bg-white active text-center">
+                                    <div className="icon"></div>
+                                    <div className="text-black left-container-title font-semibold">
+                                        Đăng tin tuyển dụng
+                                    </div>
+                                    <div className="text-sm text-neutral-700 mt-1">
+                                        Đăng tin tuyển dụng tại TopCV
+                                    </div>
+                                </div>
+                                <div className="cursor-pointer post-job-container mt-3 py-6 px-5 rounded-lg bg-white active text-center">
+                                    <div className="icon"></div>
+                                    <div className="text-black left-container-title font-semibold">
+                                        Đăng tin tuyển dụng
+                                    </div>
+                                    <div className="text-sm text-neutral-700 mt-1">
+                                        Đăng tin tuyển dụng tại TopCV
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-7 w-[60%] bg-white rounded-lg py-4 ">
+                                <div className="bg-white my-12 flex flex-col  justify-center align-middle items-center text-center ">
+                                    <img
+                                        width={128}
+                                        height={128}
+                                        src="https://tuyendung.topcv.vn/app/_nuxt/img/campaign_search_cv.82a7863.png"
+                                        alt="find job"
+                                    />
+                                    <div className="font-semibold text-black mt-2">
+                                        Tìm CV ứng viên
+                                    </div>
+                                    <div className="text-sm text-neutral-700 mt-1">
+                                        Chủ động tìm CV và liên hệ phỏng vấn với
+                                        <br />
+                                        những ứng viên phù hợp
+                                    </div>
+                                    <div className="cursor-pointer font-medium text-white mt-3 py-2 px-4 rounded-md bg-[#00b14f] ">
+                                        Tìm CV -&gt;
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
