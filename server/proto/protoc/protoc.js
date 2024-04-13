e5ee409423c13424c173724ffc6bce266ea9fb82
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-04-13 15:32:52 +0700

    add protoc script for other os

---
+++ server/proto/protoc/protoc.js
@@ -0,0 +1,13 @@
+const process = require('process');
+
+if (process.argv.length < 3) {
+    console.error('Error: Missing filename argument.');
+    process.exit(1); // Exit with non-zero code to indicate error
+}
+
+const fileName = process.argv[2];
+
+const protocCommand = `protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./libs/common/src --ts_proto_opt=nestJs=true ./proto/${fileName}.proto`;
+
+// You can execute the command here using child_process
+require('child_process').execSync(protocCommand);
