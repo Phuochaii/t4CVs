cad3bb58b403cfb1cdcfb0d0513b42eeb5b48c61
20120467 <phuochainguyen2012@gmail.com>
2024-04-14 22:39:22 +0700

    fix: add major entity

--- server/apps/gateway/src/job/job.service.ts
+++ server/apps/gateway/src/job/job.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateJobDto } from './dto/Req/createJob.dto';
 import { Observable } from 'rxjs';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateMajorDto } from './dto/Req/createMajor.dto';
 
 @Injectable()
 export class JobService {
@@ -22,5 +23,13 @@ export class JobService {
 
   findJobById(id: number): Observable<string> {
     return this.jobClient.send({ cmd: 'find_job_by_id' }, id);
+  }
+
+  createMajor(major: CreateMajorDto): Observable<string> {
+    return this.jobClient.send({ cmd: 'create_major' }, major);
+  }
+
+  getAllMajor(): Observable<string> {
+    return this.jobClient.send({ cmd: 'get_all_major' }, {});
   }
 }
