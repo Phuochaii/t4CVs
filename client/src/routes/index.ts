1e4c1cd4decd854658eb685659d9f41db4a75c36
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2024-04-16 16:03:00 +0700

    update 1st UI task division

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -13,30 +13,28 @@ const routes = [
   {path: 'hr-signup', component:Authentication.HRSignUp, layout: EmptyLayout},
   {path: 'user-login', component:Authentication.UserLogIn, layout: EmptyLayout},
   {path: 'user-signup', component:Authentication.UserSignUp, layout: EmptyLayout},
-  // khuc nay tien them vo nha
 
   // USER PAGES - Nguyên , Khoa, Hùng
-  { path: "/", component: User.Home, layout: UserLayout },
-  { path: "/results", component: User.SearchJob, layout: UserLayout },
+  { path: "/", component: User.Home, layout: UserLayout }, //khoa
+  { path: "/results", component: User.SearchJob, layout: UserLayout }, // khoa
   {
     path: "/your-application",
     component: User.YourApplications,
     layout: UserLayout,
-  },
-  { path: "/apply", component: User.ApplyCV, layout: UserLayout },
-  { path: "/create-cv", component: User.CreateCV, layout: UserLayout },
-  { path: "/edit-cv", component: User.EditCV, layout: UserLayout },
-  { path: "/manage-cv", component: User.ManageCV, layout: UserLayout },
-  {path: "/list-cv", component: User.ListCV, layout: UserLayout},
+  },// hùng
+  { path: "/apply", component: User.ApplyCV, layout: UserLayout },//khoa
+  { path: "/create-cv", component: User.CreateCV, layout: UserLayout },//
+  { path: "/edit-cv", component: User.EditCV, layout: UserLayout },//
+  { path: "/manage-cv", component: User.ManageCV, layout: UserLayout }, //nguyên
+  {path: "/list-cv", component: User.ListCV, layout: UserLayout}, // nguyên
 
   // ADMIN PAGES - Thức
   { path: "/admin/agents", component: Admin.Agents, layout: AdminLayout },
   { path: "/admin/compaign", component: Admin.Compaign, layout: AdminLayout },
 
   // HR PAGES - Yến , Thịnh
-  { path: "/hr/post-job", component: HR.PostJob, layout: HRLayout },
-  { path: "/hr/approve", component: HR.ReceiveCV, layout: HRLayout },
-
+  { path: "/hr/post-job", component: HR.PostJob, layout: HRLayout }, // thinh
+  { path: "/hr/approve", component: HR.ReceiveCV, layout: HRLayout }, // yến
   // ERROR PAGES
   { path: "error-path", component: Error.ErrorPath, layout: EmptyLayout },
 ];
