5494369841fd9ece7cd9c61437e97fceb9dcc462
tearecoil <ldthuc0605@gmail.com>
2024-04-17 13:34:59 -0600

    Added padding to admin header

--- client/src/layouts/AdminLayout/components/Header.tsx
+++ client/src/layouts/AdminLayout/components/Header.tsx
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center h-[10vh] gap-6">
+    <div className="px-4 flex justify-between items-center h-[10vh] gap-6">
       <div className="flex-1 h-full">
         <img
           src="/topcv-logo-6.webp"
