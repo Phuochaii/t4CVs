6a1508a3b217672b5010d10598b733f3083d96c4
Tiến Nguyễn <72902646+nguyenductien71311@users.noreply.github.com>
2024-04-21 15:23:50 +0700

    fix: Fix gateway module
    Add Company Module in gateway

--- server/apps/gateway/src/gateway.module.ts
+++ server/apps/gateway/src/gateway.module.ts
@@ -6,12 +6,14 @@ import { UploadModule } from './upload/upload.module';
 
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { Company } from 'apps/company-service/src/entities/company.entity';
 
 @Module({
   imports: [
     ApplicationModule,
     // JobModule,
     // UploadModule,
+    CompanyModule,
   ],
   controllers: [GatewayController],
   providers: [GatewayService],
