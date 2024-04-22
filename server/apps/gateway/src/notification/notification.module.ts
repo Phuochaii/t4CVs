812e2668136dc2c6a8d989e23adf4b11158ddd76
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-04-22 20:40:52 +0700

    [notification--service] feat: gateway notification service

--- server/apps/gateway/src/notification/notification.module.ts
+++ server/apps/gateway/src/notification/notification.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NOTIFICATION_PACKAGE_NAME } from '@app/common/proto/notification';
+import { join } from 'path';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: NOTIFICATION_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: 'localhost:50052',
+          package: NOTIFICATION_PACKAGE_NAME,
+          protoPath: join(__dirname, './proto/application.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
--- server/apps/gateway/src/notification/notification.service.ts
+++ server/apps/gateway/src/notification/notification.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { request } from 'http';
+import { NOTIFICATION_PACKAGE_NAME, NOTIFICATION_SERVICE_NAME, NotificationServiceClient, SendNotificationRequest, status } from '@app/common/proto/notification';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { PaginationRequest } from '@app/common';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService implements OnModuleInit {
+    private notificationServiceClient: NotificationServiceClient;
+    constructor(
+        @Inject(NOTIFICATION_PACKAGE_NAME) private readonly client: ClientGrpc,
+    ) { }
+    onModuleInit() {
+        this.notificationServiceClient = this.client.getService<NotificationServiceClient>(NOTIFICATION_SERVICE_NAME);
+    }
+
+    create(request: SendNotificationRequest) {
+        return this.notificationServiceClient.sendNotification(request);
+    }
+
+    getAllOfUser(userId: number, paginationRequest: PaginationRequest) {
+        return this.notificationServiceClient.getNotifications({
+            user: { id: userId },
+            paginationRequest,
+        });
+    }
+
+    updateStatus(userId: number, notificationId: number, status: status) {
+        return this.notificationServiceClient.updateNotificationStatus({
+            user: { id: userId },
+            notificationId,
+            status,
+        });
+    }
+}
