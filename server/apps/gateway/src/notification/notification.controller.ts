01d1d2de30a74ae35f0c6e02de8001b57fb3bf18
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-04-23 14:07:05 +0700

    [notification--service] feat: gateway get all user's notifications api

--- server/apps/gateway/src/notification/notification.controller.ts
+++ server/apps/gateway/src/notification/notification.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { SendNotificationRequest } from '@app/common/proto/notification';
+import { PaginationRequest } from '@app/common';
 
 @Controller('notification')
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+  constructor(private readonly notificationService: NotificationService) { }
 
-  @Post()
-  create(@Body() request: SendNotificationRequest){
-    return this.notificationService.create(request);
+  @Get("/:userId")
+  getAllOfUser(
+    @Param('userId') userId: number,
+    @Query() paginationRequest: PaginationRequest = new PaginationRequest()
+  ) {
+    return this.notificationService.getAllOfUser(userId, paginationRequest);
   }
 }
