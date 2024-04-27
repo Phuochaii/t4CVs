c6328eb0cd7828e8d1e6b95e3d10d276f548cbdc
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-04-27 07:34:01 +0700

    [notification] refactor: using .env variable

--- server/apps/notification-service/src/notification-service.module.ts
+++ server/apps/notification-service/src/notification-service.module.ts
@@ -1,14 +1,34 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { NotificationController } from './notification-service.controller';
 import { NotificationServiceService } from './services';
 import { Notification, User_Notification } from './entities';
-import { databaseConfig } from './database/init';
+import { DatabaseConfiger, DatabaseOptions } from './database/init';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import path from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: path.resolve(__dirname, '.env')
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: databaseConfig,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const defaultConfig : DatabaseOptions = {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: `${configService.get('DB_PASSWORD')}`,
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+        const databaseConfiger = new DatabaseConfiger(defaultConfig);
+        return databaseConfiger.config();
+      }
     }),
     TypeOrmModule.forFeature([Notification, User_Notification]),
   ],
