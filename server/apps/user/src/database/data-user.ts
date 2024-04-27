9bacf0f800ef078748957c648a3584dd00d4c6d9
20120467 <phuochainguyen2012@gmail.com>
2024-04-27 22:20:48 +0700

    fix: auto create data in user service

---
+++ server/apps/user/src/database/data-user.ts
@@ -0,0 +1,64 @@
+import { User } from '../entities/user.entity';
+
+export const users: User[] = [
+  {
+    id: 1,
+    fullname: 'John Doe',
+    phone: '123-456-7890',
+    image: 'john_doe.jpg',
+  },
+  {
+    id: 2,
+    fullname: 'Jane Smith',
+    phone: '987-654-3210',
+    image: 'jane_smith.jpg',
+  },
+  {
+    id: 3,
+    fullname: 'Michael Johnson',
+    phone: '555-555-5555',
+    image: 'michael_johnson.jpg',
+  },
+  {
+    id: 4,
+    fullname: 'Emily Davis',
+    phone: '111-222-3333',
+    image: 'emily_davis.jpg',
+  },
+  {
+    id: 5,
+    fullname: 'Daniel Brown',
+    phone: '444-444-4444',
+    image: 'daniel_brown.jpg',
+  },
+  {
+    id: 6,
+    fullname: 'Sarah Wilson',
+    phone: '777-777-7777',
+    image: 'sarah_wilson.jpg',
+  },
+  {
+    id: 7,
+    fullname: 'Matthew Martinez',
+    phone: '999-999-9999',
+    image: 'matthew_martinez.jpg',
+  },
+  {
+    id: 8,
+    fullname: 'Emma Taylor',
+    phone: '666-666-6666',
+    image: 'emma_taylor.jpg',
+  },
+  {
+    id: 9,
+    fullname: 'Christopher Anderson',
+    phone: '333-333-3333',
+    image: 'christopher_anderson.jpg',
+  },
+  {
+    id: 10,
+    fullname: 'Olivia Thomas',
+    phone: '888-888-8888',
+    image: 'olivia_thomas.jpg',
+  },
+];
