190cbb2ab68ac03b7823de29e0f8d70c7b0454cb
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2024-04-28 10:26:40 +0700

    add cors: true

--- server/apps/gateway/src/main.ts
+++ server/apps/gateway/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
+  const app = await NestFactory.create(GatewayModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
