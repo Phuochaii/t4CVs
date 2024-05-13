fecf624f29861c9cc4fea351b9300501f9b535a2
tearecoil <ldthuc0605@gmail.com>
2024-05-12 19:22:11 -0600

    Added user info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,21 +7,51 @@ import {
 
 import routes from "./routes";
 import EmptyLayout from "./layouts/EmptyLayout";
-import React, { createContext } from "react";
+import { createContext } from "react";
 
 export const MyContext = createContext({
   accountList: [
-    { id: "", role: "admin", email: "admin@gmail.com", password: "123456" },
-    { id: "1", role: "hr", email: "hr@gmail.com", password: "123456" },
-    { id: "1", role: "user", email: "user@gmail.com", password: "123456" },
+    {
+      id: "",
+      role: "admin",
+      email: "admin@gmail.com",
+      password: "123456",
+    },
+    {
+      id: "1",
+      role: "hr",
+      email: "hr@gmail.com",
+      password: "123456",
+    },
+    {
+      id: "1",
+      role: "user",
+      email: "user@gmail.com",
+      password: "123456",
+    },
   ],
 });
 
 function App() {
   const accountList = [
-    { role: "admin", id: "", email: "admin@gmail.com", password: "123456" },
-    { role: "hr", id: "1", email: "hr@gmail.com", password: "123456" },
-    { role: "user", id: "1", email: "user@gmail.com", password: "123456" },
+    {
+      role: "admin",
+      id: "",
+      email: "admin@gmail.com",
+      password: "123456",
+    },
+    {
+      role: "hr",
+      id: "1",
+      email: "hr@gmail.com",
+      password: "123456",
+    },
+    {
+      role: "user",
+      id: "1",
+      email: "user@gmail.com",
+      password: "123456",
+    },
   ];
 
   console.log(accountList);
