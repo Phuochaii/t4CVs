2452e8cc9694264aa2067f1f60d8081fbff141a4
Trịnh Quốc Cường <cuongparish@gmail.com>
2024-05-23 14:53:11 +0700

    refactor: change from controller -> service
    [Application Service]

--- server/apps/gateway/src/application/application.controller.ts
+++ server/apps/gateway/src/application/application.controller.ts
@@ -6,17 +6,12 @@ import {
   Patch,
   Param,
   Query,
-  ForbiddenException,
   ParseBoolPipe,
 } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { CompanyService } from '../company/company.service';
 import { UserService } from '../user/user.service';
-import {
-  NotificationService,
-  NotificationUserId,
-  NotificationUserRole,
-} from '../notification/notification.service';
+import { NotificationService } from '../notification/notification.service';
 import { CreateApplicationRequest } from '@app/common/proto/application';
 import { firstValueFrom } from 'rxjs';
 
@@ -33,25 +28,7 @@ export class ApplicationController {
 
   @Post()
   async create(@Body() createApplicationRequest: CreateApplicationRequest) {
-    const application = await firstValueFrom(
-      this.applicationService.create(createApplicationRequest),
-    );
-
-    const campaign = await firstValueFrom(
-      this.companyService.findCampaignById(createApplicationRequest.campaignId),
-    );
-    const employerId = campaign.employerId;
-
-    const notification = await firstValueFrom(
-      this.notificationService.create(
-        [new NotificationUserId(employerId, NotificationUserRole.HR)],
-        {
-          content: `Ứng viên ${application.fullname}- ${campaign.name}`,
-          link: `application/${application.id}`,
-          title: `CV mới ứng tuyển`,
-        },
-      ),
-    );
+    await this.applicationService.create(createApplicationRequest);
     return 'Success';
   }
 
