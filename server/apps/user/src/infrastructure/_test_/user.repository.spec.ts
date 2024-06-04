a853de334c340d952d378d1cd26613da36ae4041
20120467 <phuochainguyen2012@gmail.com>
2024-06-04 23:31:22 +0700

    style: remove unuse package

--- server/apps/user/src/infrastructure/_test_/user.repository.spec.ts
+++ server/apps/user/src/infrastructure/_test_/user.repository.spec.ts
@@ -1,6 +1,5 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { TypeOrmUserRepository } from '../repository/user.repository';
 import { User } from '../schemas/user.schema';
 import { CreateUserDTO } from '../../domain/dto/Req';
