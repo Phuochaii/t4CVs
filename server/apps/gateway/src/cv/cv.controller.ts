86c7987bdd81329ebac0a80d694dd3234c379f1c
Trịnh Quốc Cường <cuongparish@gmail.com>
2024-06-04 15:09:35 +0700

    fix role method find one by id

--- server/apps/gateway/src/cv/cv.controller.ts
+++ server/apps/gateway/src/cv/cv.controller.ts
@@ -10,6 +10,7 @@ import {
   UseInterceptors,
   Res,
   UseGuards,
+  ForbiddenException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { CVService } from './cv.service';
@@ -18,6 +19,8 @@ import { diskStorage } from 'multer';
 import { Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
 import { PermissionsGuard } from '../authorization/permission/permissions.guard';
+import { UserClaims } from '../authorization/entity/user-claims.entity';
+import { GetUser } from '../authorization/get-user.decorator';
 
 @Controller('cv')
 export class CVController {
@@ -39,8 +42,17 @@ export class CVController {
   //find one by id -> hr + user
   @UseGuards(AuthGuard('jwt'))
   @Get(':id')
-  getCVById(@Param('id') id: number): Observable<any> {
-    return this.cvService.getCVById(id);
+  getCVById(
+    @GetUser() user: UserClaims,
+    @Param('id') id: number,
+  ): Observable<any> {
+    const hasRole = user.permissions;
+    if (hasRole[0] === 'role:hr' || hasRole[0] === 'role:user') {
+      return this.cvService.getCVById(id);
+    }
+    throw new ForbiddenException(
+      'You are not authorized to access this resource',
+    );
   }
   //create cv
   @UseGuards(AuthGuard('jwt'), PermissionsGuard('role:user'))
