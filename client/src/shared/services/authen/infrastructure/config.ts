bee283c29ff82b781918a181bba1b9633324f93d
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2024-06-16 19:57:58 +0700

    Update config.ts

--- client/src/shared/services/authen/infrastructure/config.ts
+++ client/src/shared/services/authen/infrastructure/config.ts
@@ -1,4 +1,4 @@
-export const HOST_URL = 'https://dacnpm-m9tw.onrender.com';
+export const HOST_URL = window.location.origin;
 export const AUTH0_DOMAIN = 'dev-0oh0fi1avbej1681.us.auth0.com';
 export const AUTH0_CLIENT_ID = 'NXdIWANcMLVivHz2A3ANsPHLYVvCAgd1';
 export const AUTH0_LOGIN_REDIRECT_URL = HOST_URL;
@@ -8,7 +8,7 @@ export enum AUTH0_REALM {
     Facebook = 'facebook',
     LinkedIn = 'linkedin',
 } ;
-export const AUTH0_SCOPE = 'openid email profile offline_access';
+export const AUTH0_SCOPE = 'openid email profile offline_access';a
 export const AUTH0_LOGIN_RESPONSE_TYPE = 'code';
 export const AUTH0_LOGOUT_URL = HOST_URL;
 export const AUTH0_BACKEND_AUDIENCE = 'http://localhost:300/';
